5ce0527d4074c76bb9265d40730d2897c101a73d
Monatric <nawpnawp8@gmail.com>
2024-06-14 23:32:48 +0800

    Finish the project, returning two values in array representing the days
    The logic of profit variable is changed. The second day must represent the sell day and the first day represents the buy day. Because you wouldn't call it a profit if it's higher "buy" and lower "sell". That and #abs is removed to find the negative returns.
    
    Another logic is added to keep the sell days from going before the buy days.

--- script.rb
+++ script.rb
@@ -1,21 +1,21 @@
 require 'pry-byebug'
 
 def stock_picker(stock_price_per_day)
+    hash = Hash.new
     stock_price_per_day.each_with_index do |first_lvl_element, first_lvl_index|
         stock_price_per_day.each_with_index do |second_lvl_element, second_lvl_index|
-            if second_lvl_index == stock_price_per_day.size - 1
+            if second_lvl_index == stock_price_per_day.size - 1 || second_lvl_index < first_lvl_index
                 next
             else
                 first_day = first_lvl_element
                 second_day = stock_price_per_day[1+second_lvl_index]
-                p "the difference of #{first_day} and #{stock_price_per_day[1+second_lvl_index]} is"
-                p profit = (first_day - second_day).abs
-                # binding.pry
-                # next is making a hash and returning a key value pairs
+                profit = second_day - first_day
+                
+                hash[[first_lvl_index, 1+second_lvl_index]] = profit
             end
         end
     end
-
+    hash.key(hash.values.max)
 end
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
